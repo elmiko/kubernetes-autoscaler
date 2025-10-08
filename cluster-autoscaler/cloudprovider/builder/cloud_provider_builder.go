kubernetes/autoscaler

refactor NewCloudProvider to accept AutoscalerOptions
this changes the options input to the cloud provider builder function so
that the full autoscaler options are passed. This is being proposed so
that cloud providers will have new options for injecting behavior into
the core parts of the autoscaler.

cluster-autoscaler/cloudprovider/builder/cloud_provider_builder.go
@@ -18,15 +18,15 @@ package builder
 
 import (
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
-	"k8s.io/autoscaler/cluster-autoscaler/config"
 	"k8s.io/autoscaler/cluster-autoscaler/context"
+	"k8s.io/autoscaler/cluster-autoscaler/core/options"
 	"k8s.io/client-go/informers"
 
 	klog "k8s.io/klog/v2"
 )
 
 // NewCloudProvider builds a cloud provider from provided parameters.
-func NewCloudProvider(opts config.AutoscalingOptions, informerFactory informers.SharedInformerFactory) cloudprovider.CloudProvider {
+func NewCloudProvider(opts *options.AutoscalerOptions, informerFactory informers.SharedInformerFactory) cloudprovider.CloudProvider {
 	klog.V(1).Infof("Building %s cloud provider.", opts.CloudProviderName)
 
 	do := cloudprovider.NodeGroupDiscoveryOptions{
@@ -34,7 +34,7 @@ func NewCloudProvider(opts config.AutoscalingOptions, informerFactory informers.
 		NodeGroupAutoDiscoverySpecs: opts.NodeGroupAutoDiscovery,
 	}
 
-	rl := context.NewResourceLimiterFromAutoscalingOptions(opts)
+	rl := context.NewResourceLimiterFromAutoscalingOptions(opts.AutoscalingOptions)
 
 	if opts.CloudProviderName == "" {
 		// Ideally this would be an error, but several unit tests of the

cluster-autoscaler/processors/scaledowncandidates/scale_down_candidates_processor.go
@@ -23,25 +23,25 @@ import (
 	"k8s.io/autoscaler/cluster-autoscaler/utils/errors"
 )
 
-type combinedScaleDownCandidatesProcessor struct {
+type CombinedScaleDownCandidatesProcessor struct {
 	processors []nodes.ScaleDownNodeProcessor
 }
 
 // NewCombinedScaleDownCandidatesProcessor returns a default implementation of the scale down candidates
 // processor, which wraps and sequentially runs other sub-processors.
-func NewCombinedScaleDownCandidatesProcessor() *combinedScaleDownCandidatesProcessor {
-	return &combinedScaleDownCandidatesProcessor{}
+func NewCombinedScaleDownCandidatesProcessor() *CombinedScaleDownCandidatesProcessor {
+	return &CombinedScaleDownCandidatesProcessor{}
 
 }
 
 // Register registers a new ScaleDownNodeProcessor
-func (p *combinedScaleDownCandidatesProcessor) Register(np nodes.ScaleDownNodeProcessor) {
+func (p *CombinedScaleDownCandidatesProcessor) Register(np nodes.ScaleDownNodeProcessor) {
 	p.processors = append(p.processors, np)
 }
 
 // GetPodDestinationCandidates returns nodes that potentially could act as destinations for pods
 // that would become unscheduled after a scale down.
-func (p *combinedScaleDownCandidatesProcessor) GetPodDestinationCandidates(ctx *context.AutoscalingContext, nodes []*apiv1.Node) ([]*apiv1.Node, errors.AutoscalerError) {
+func (p *CombinedScaleDownCandidatesProcessor) GetPodDestinationCandidates(ctx *context.AutoscalingContext, nodes []*apiv1.Node) ([]*apiv1.Node, errors.AutoscalerError) {
 	var err errors.AutoscalerError
 	for _, processor := range p.processors {
 		nodes, err = processor.GetPodDestinationCandidates(ctx, nodes)
@@ -53,7 +53,7 @@ func (p *combinedScaleDownCandidatesProcessor) GetPodDestinationCandidates(ctx *
 }
 
 // GetScaleDownCandidates returns nodes that potentially could be scaled down.
-func (p *combinedScaleDownCandidatesProcessor) GetScaleDownCandidates(ctx *context.AutoscalingContext, nodes []*apiv1.Node) ([]*apiv1.Node, errors.AutoscalerError) {
+func (p *CombinedScaleDownCandidatesProcessor) GetScaleDownCandidates(ctx *context.AutoscalingContext, nodes []*apiv1.Node) ([]*apiv1.Node, errors.AutoscalerError) {
 	var err errors.AutoscalerError
 	for _, processor := range p.processors {
 		nodes, err = processor.GetScaleDownCandidates(ctx, nodes)
@@ -65,7 +65,7 @@ func (p *combinedScaleDownCandidatesProcessor) GetScaleDownCandidates(ctx *conte
 }
 
 // CleanUp is called at CA termination
-func (p *combinedScaleDownCandidatesProcessor) CleanUp() {
+func (p *CombinedScaleDownCandidatesProcessor) CleanUp() {
 	for _, processor := range p.processors {
 		processor.CleanUp()
 	}

elmiko/kubernetes-autoscaler

cluster-autoscaler/cloudprovider/builder/cloud_provider_builder.go
@@ -18,15 +18,15 @@ package builder
 
 import (
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
-	"k8s.io/autoscaler/cluster-autoscaler/config"
 	ca_context "k8s.io/autoscaler/cluster-autoscaler/context"
+	"k8s.io/autoscaler/cluster-autoscaler/core/options"
 	"k8s.io/client-go/informers"
 
 	klog "k8s.io/klog/v2"
 )
 
 // NewCloudProvider builds a cloud provider from provided parameters.
-func NewCloudProvider(opts config.AutoscalingOptions, informerFactory informers.SharedInformerFactory) cloudprovider.CloudProvider {
+func NewCloudProvider(opts *options.AutoscalerOptions, informerFactory informers.SharedInformerFactory) cloudprovider.CloudProvider {
 	klog.V(1).Infof("Building %s cloud provider.", opts.CloudProviderName)
 
 	do := cloudprovider.NodeGroupDiscoveryOptions{
@@ -34,7 +34,7 @@ func NewCloudProvider(opts config.AutoscalingOptions, informerFactory informers.
 		NodeGroupAutoDiscoverySpecs: opts.NodeGroupAutoDiscovery,
 	}
 
-	rl := ca_context.NewResourceLimiterFromAutoscalingOptions(opts)
+	rl := ca_context.NewResourceLimiterFromAutoscalingOptions(opts.AutoscalingOptions)
 
 	if opts.CloudProviderName == "" {
 		// Ideally this would be an error, but several unit tests of the

cluster-autoscaler/processors/scaledowncandidates/scale_down_candidates_processor.go
@@ -23,25 +23,25 @@ import (
 	"k8s.io/autoscaler/cluster-autoscaler/utils/errors"
 )
 
-type combinedScaleDownCandidatesProcessor struct {
+type CombinedScaleDownCandidatesProcessor struct {
 	processors []nodes.ScaleDownNodeProcessor
 }
 
 // NewCombinedScaleDownCandidatesProcessor returns a default implementation of the scale down candidates
 // processor, which wraps and sequentially runs other sub-processors.
-func NewCombinedScaleDownCandidatesProcessor() *combinedScaleDownCandidatesProcessor {
-	return &combinedScaleDownCandidatesProcessor{}
+func NewCombinedScaleDownCandidatesProcessor() *CombinedScaleDownCandidatesProcessor {
+	return &CombinedScaleDownCandidatesProcessor{}
 
 }
 
 // Register registers a new ScaleDownNodeProcessor
-func (p *combinedScaleDownCandidatesProcessor) Register(np nodes.ScaleDownNodeProcessor) {
+func (p *CombinedScaleDownCandidatesProcessor) Register(np nodes.ScaleDownNodeProcessor) {
 	p.processors = append(p.processors, np)
 }
 
 // GetPodDestinationCandidates returns nodes that potentially could act as destinations for pods
 // that would become unscheduled after a scale down.
-func (p *combinedScaleDownCandidatesProcessor) GetPodDestinationCandidates(autoscalingCtx *ca_context.AutoscalingContext, nodes []*apiv1.Node) ([]*apiv1.Node, errors.AutoscalerError) {
+func (p *CombinedScaleDownCandidatesProcessor) GetPodDestinationCandidates(autoscalingCtx *ca_context.AutoscalingContext, nodes []*apiv1.Node) ([]*apiv1.Node, errors.AutoscalerError) {
 	var err errors.AutoscalerError
 	for _, processor := range p.processors {
 		nodes, err = processor.GetPodDestinationCandidates(autoscalingCtx, nodes)
@@ -53,7 +53,7 @@ func (p *combinedScaleDownCandidatesProcessor) GetPodDestinationCandidates(autos
 }
 
 // GetScaleDownCandidates returns nodes that potentially could be scaled down.
-func (p *combinedScaleDownCandidatesProcessor) GetScaleDownCandidates(autoscalingCtx *ca_context.AutoscalingContext, nodes []*apiv1.Node) ([]*apiv1.Node, errors.AutoscalerError) {
+func (p *CombinedScaleDownCandidatesProcessor) GetScaleDownCandidates(autoscalingCtx *ca_context.AutoscalingContext, nodes []*apiv1.Node) ([]*apiv1.Node, errors.AutoscalerError) {
 	var err errors.AutoscalerError
 	for _, processor := range p.processors {
 		nodes, err = processor.GetScaleDownCandidates(autoscalingCtx, nodes)
@@ -65,7 +65,7 @@ func (p *combinedScaleDownCandidatesProcessor) GetScaleDownCandidates(autoscalin
 }
 
 // CleanUp is called at CA termination
-func (p *combinedScaleDownCandidatesProcessor) CleanUp() {
+func (p *CombinedScaleDownCandidatesProcessor) CleanUp() {
 	for _, processor := range p.processors {
 		processor.CleanUp()
 	}